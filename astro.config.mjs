07837962a45db86861db37ec919193a5a5a9740a
tomioM <tomiowmiyagawa@gmail.com>
2023-08-04 19:48:34 -0700

    Bookshop added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
+import bookshop from '@bookshop/astro-bookshop';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [bookshop()]
+});
