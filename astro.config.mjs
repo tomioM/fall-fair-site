c9b22389106fbcf9f8ec4b9883733221012564e9
tomioM <tomiowmiyagawa@gmail.com>
2023-08-05 16:48:49 -0700

    Revert to static site

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import bookshop from '@bookshop/astro-bookshop';
-
-// https://astro.build/config
-export default defineConfig({
-    integrations: [bookshop()]
-});
